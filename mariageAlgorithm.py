da98702244f410ca48513c9e48075d85ba758769
mathieu <m.teissedre31@gmail.com>
2020-01-22 15:48:04 +0100

    algorithme de mariage terminé

--- mariageAlgorithm.py
+++ mariageAlgorithm.py
@@ -4,7 +4,7 @@ def getKey(elements, search):
              return key 
     return "no key"
 
-def mariageStable (eleves, ecoles, nbEleves) :
+def mariageStableEleves (eleves, ecoles, nbEleves) :
     balcons = {}
     positions = nbEleves
     for key in eleves.keys():
@@ -32,7 +32,35 @@ def mariageStable (eleves, ecoles, nbEleves) :
                             student = getKey(ecoles[element],positions[element])
                             if student != "no key" :
                                 balcons[student].append(element)
-    return balcons       
+    return balcons  
+
+def mariageStableEcoles (eleves, ecoles, nbEleves) :
+    balcons = {}
+    for key in ecoles.keys():
+        balcons[key] = []
+    for key in eleves.keys() :
+        school = getKey(eleves[key],1)
+        if school != "no key" :
+            balcons[school].append(key)
+    stable = False
+    while not stable :
+        stable = True
+        for key in balcons.keys() :
+            if len(balcons[key]) > nbEleves[key] :
+                stable = False
+                while len(balcons[key]) > nbEleves[key] :
+                    maximum = 0
+                    for element in balcons[key] :
+                        if maximum == 0 or eleves[element][key] > maximum :
+                            maximum = eleves[element][key]
+                    for element in balcons[key] :
+                        if ecoles[key][element] == maximum :
+                            balcons[key].remove(element)
+                            position = eleves[element][key] + 1
+                            ecole = getKey(eleves[element],position)
+                            if ecole != "no key" :
+                                balcons[ecole].append(element)
+    return balcons      
 
 
 eleves = {"Mathieu" : {"ENSEEIHT":1,"ENSIBS":3,"ENSIMAG":2},
@@ -46,4 +74,7 @@ ecoles = {"ENSEEIHT":{"Ignace":1,"Mathieu":2,"Thomas":3, "Tom":4},
 
 nbEleves = {"ENSEEIHT": 2, "ENSIMAG" : 1, "ENSIBS" : 3}
 
-print(mariageStable(eleves, ecoles, nbEleves))
+print("Eleves favorisés :")
+print(mariageStableEleves(eleves, ecoles, nbEleves))
+print("Ecoles favorisés :")
+print(mariageStableEcoles(eleves, ecoles, nbEleves))
